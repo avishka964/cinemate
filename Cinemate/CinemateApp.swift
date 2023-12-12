d2b887e86a5a1fd7b262cd9cb093cf0e2046f6ad
Avishka <avishkakapuruge@gmail.com>
2023-12-12 21:26:37 +0530

    Initial Commit

---
+++ Cinemate/CinemateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemateApp.swift
+//  Cinemate
+//
+//  Created by Avishka Kapuruge on 2023-12-12.
+//
+
+import SwiftUI
+
+@main
+struct CinemateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cinemate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cinemate
+//
+//  Created by Avishka Kapuruge on 2023-12-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

5e4346bdb86aada6d5ebfe51f93c1fd3be49bbd9
Avishka <avishkakapuruge@gmail.com>
2023-12-16 19:35:07 +0530

    firebase integration

---
+++ Cinemate/Others/CustomFont.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFont.swift
+//  Cinemate
+//
+//  Created by Avishka Kapuruge on 2023-12-16.
+//
+
+import Foundation
+
+struct CustomFont {
+    struct Roboto {
+        static let bold = "Roboto-Bold"
+        static let light = "Roboto-Light"
+        static let medium = "Roboto-Medium"
+        static let regular = "Roboto-Regular"
+    }
+}
